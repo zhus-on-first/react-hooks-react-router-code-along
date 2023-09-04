5474579dcdd9282651b4b5787cc2bf83a8cb98d2
Jia-Jia Zhu <dodge_waking_0b@icloud.com>
2023-09-04 14:26:49 -0700

    all components, navlinks, routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,63 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, NavLink, Link } from "react-router-dom";
+
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
+
+function NavBar() {
+  return (
+    <div>
+      <NavLink
+        to="/"
+        exact
+        style={linkStyles}
+        activeStyle={{ background: "darkblue" }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        exact
+        style={linkStyles}
+        activeStyle={{ background: "darkblue" }}
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="/login"
+        exact
+        style={linkStyles}
+        activeStyle={{ background: "darkblue" }}
+      >
+        Login
+      </NavLink>
+      <NavLink
+        to="/signup"
+        exact
+        style={linkStyles}
+        activeStyle={{ background: "darkblue" }}
+      >
+        Signup
+      </NavLink>
+      <NavLink
+        to="/messages"
+        exact
+        style={linkStyles}
+        activeStyle={{ background: "darkblue" }}
+      >
+        Messages
+      </NavLink>
+    </div>
+  );
+}
 
 function Home() {
   return (
@@ -9,8 +67,67 @@ function Home() {
   );
 }
 
-function App() {
-  return <Home />;
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+function Signup() {
+  return <h1>Signup</h1>;
+}
+
+function Messages() {
+  return <h1>Messages</h1>;
+}
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/messages">
+          <Messages />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
